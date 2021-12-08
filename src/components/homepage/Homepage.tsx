1f6951e82ad71670c6f3bcdc34c23f05af7764a1
Nicholas Ledan <Nickledan5@gmail.com>
2021-12-08 17:07:02 -0500

    added Logos

--- src/components/homepage/Homepage.tsx
+++ src/components/homepage/Homepage.tsx
@@ -1,6 +1,12 @@
 import React, { useEffect, useState} from 'react';
 import "./homepage.css"
-import tsLogo from '../shared/TSlogo.png'
+import  tsLogo  from '../shared/TSlogo.png'
+import jsLogo from '../shared/javascript.png'
+import pyLogo from '../shared/python.png'
+import htmlLogo from '../shared/html.png'
+import cssLogo from '../shared/css3.png'
+
+
 
 export default function HomePage() {
   let [users, updateUsers] = useState(0)
@@ -15,6 +21,10 @@ export default function HomePage() {
       <button onClick={() => {updateUsers(users + 1)}}>Add User</button>
       <br />
       <img src={tsLogo} alt="TypeScript logo" className="logo"/>
+      <img src={jsLogo} alt="TypeScript logo" className="logo"/>
+      <img src={pyLogo} alt="TypeScript logo" className="logo"/>
+      <img src={htmlLogo} alt="TypeScript logo" className="logo"/>
+      <img src={cssLogo} alt="TypeScript logo" className="logo"/>
     </>
   )
 }
